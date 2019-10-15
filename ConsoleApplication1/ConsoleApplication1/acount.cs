6de93d041d04d0eb1b9d395355386c70ee72566b
trandinhnhan0612 <trandinhnhan9958@gmail.com>
2019-10-15 17:36:49 +0700

    trandinhnhan

---
+++ ConsoleApplication1/ConsoleApplication1/acount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class acount
+    {
+        private int MONEY;
+        public acount(int amount)
+        {
+            MONEY = amount;
+
+        }
+        public void Deposit(int amount)
+        {
+            MONEY += amount;
+        }
+        public void WithDraw(int amount)
+        {
+            MONEY += amount;
+        }
+        public  int Balance()
+        {
+            return MONEY;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTestProject1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestProject1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            acount acc = new acount(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(acount));
+            Assert.AreEqual(200000, acc.Balance());
+
+        }
+    }
+}
